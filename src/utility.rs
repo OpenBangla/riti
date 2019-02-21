5968e69124424d719f14aa6ba49e2b79b971adea
Muhammad Mominul Huque <mominul2082@gmail.com>
2019-02-21 14:35:09 +0600

    Optimization: Remove unnecessary string allocation. Add tests.

--- src/utility.rs
+++ src/utility.rs
@@ -1,6 +1,6 @@
 use regex::Regex;
 
-lazy_static!{
+lazy_static! {
     static ref VOWEL: Regex = Regex::new(r#"^[\u0985\u0986\u0987\u0988\u0989\u098A\u098B\u098F\u0990\u0993\u0994\u098C\u09E1\u09BE\u09BF\u09C0\u09C1\u09C2\u09C3\u09C7\u09C8\u09CB\u09CC]$"#).unwrap();
     static ref KAR: Regex = Regex::new(r#"^[\u09BE\u09BF\u09C0\u09C1\u09C2\u09C3\u09C7\u09C8\u09CB\u09CC\u09C4]$"#).unwrap();
     static ref CONSONANT: Regex = Regex::new(r#"^[\u0995\u0996\u0997\u0998\u0999\u099A\u099B\u099C\u099D\u099E\u099F\u09A0\u09A1\u09A2\u09A3\u09A4\u09A5\u09A6\u09A7\u09A8\u09AA\u09AB\u09AC\u09AD\u09AE\u09AF\u09B0\u09B2\u09B6\u09B7\u09B8\u09B9\u09CE\u09DC\u09DD\u09DF]$"#).unwrap();
@@ -37,16 +37,12 @@ impl Utility for str {
 
     fn split_string(&self) -> (String, String, String) {
         let meta = "-]~!@#%&*()_=+[{}'\";<>/?|.,";
-        let mut first_part = String::new();
         let mut first_index = 0;
-        let mut last_part = String::new();
         let mut last_index = 0;
         let mut encountered_alpha = false;
 
         for (index, c) in self.chars().enumerate() {
-            if meta.contains(c) {
-                first_part.push(c);
-            } else {
+            if !meta.contains(c) {
                 first_index = index;
                 encountered_alpha = true;
                 break;
@@ -57,20 +53,19 @@ impl Utility for str {
         // a numeric character, then the string has no middle part
         // or last part we need. So return "" for them ;)
         if !encountered_alpha {
-            return (first_part, "".to_owned(), "".to_owned());
+            return (self[..].to_owned(), "".to_owned(), "".to_owned());
         }
 
         for (index, c) in self.chars().rev().enumerate() {
-            if meta.contains(c) {
-                // Insert it in reverse.
-                last_part.insert(0, c);
-            } else {
+            if !meta.contains(c) {
                 last_index = self.len() - index - 1;
                 break;
             }
         }
 
+        let first_part = self[0..first_index].to_owned();
         let middle_part = self[first_index..=last_index].to_owned();
+        let last_part = self[last_index + 1..].to_owned();
 
         (first_part, middle_part, last_part)
     }
@@ -90,9 +85,33 @@ mod test {
 
     #[test]
     fn test_split_string() {
-        assert_eq!("[][][][]".split_string(), ("[][][][]".to_owned(), "".to_owned(), "".to_owned()));
-        assert_eq!("t*".split_string(), ("".to_owned(), "t".to_owned(), "*".to_owned()));
-        assert_eq!("1".split_string(), ("".to_owned(), "1".to_owned(), "".to_owned()));
-        assert_eq!("#\"percent%sign\"#".split_string(), ("#\"".to_owned(), "percent%sign".to_owned(), "\"#".to_owned()));
+        assert_eq!(
+            "[][][][]".split_string(),
+            ("[][][][]".to_owned(), "".to_owned(), "".to_owned())
+        );
+        assert_eq!(
+            "t*".split_string(),
+            ("".to_owned(), "t".to_owned(), "*".to_owned())
+        );
+        assert_eq!(
+            "1".split_string(),
+            ("".to_owned(), "1".to_owned(), "".to_owned())
+        );
+        assert_eq!(
+            "#\"percent%sign\"#".split_string(),
+            (
+                "#\"".to_owned(),
+                "percent%sign".to_owned(),
+                "\"#".to_owned()
+            )
+        );
+        assert_eq!(
+            "text".split_string(),
+            ("".to_owned(), "text".to_owned(), "".to_owned())
+        );
+        assert_eq!(
+            ":)".split_string(),
+            ("".to_owned(), ":".to_owned(), ")".to_owned())
+        );
     }
 }
