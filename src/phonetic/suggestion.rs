36d510944563242590603a6d937887d2c68d65eb
Muhammad Mominul Huque <mominul2082@gmail.com>
2019-03-05 00:47:02 +0600

    Implement Utility trait on char not on str. split split_string() function

--- src/phonetic/suggestion.rs
+++ src/phonetic/suggestion.rs
@@ -31,17 +31,27 @@ impl PhoneticSuggestion {
                     let key = &middle[0..(middle.len() - suffix_key.len())];
                     if self.cache.contains_key(key) {
                         for item in &self.cache[key] {
-                            let item_rmc = item.chars().last().unwrap().to_string(); // Right most character.
-                            let suffix_lmc = suffix.chars().nth(0).unwrap().to_string(); // Left most character.
+                            let item_rmc = item.chars().last().unwrap(); // Right most character.
+                            let suffix_lmc = suffix.chars().nth(0).unwrap(); // Left most character.
                             if item_rmc.is_vowel() && suffix_lmc.is_kar() {
                                 let word = format!("{}{}{}", item, "\u{09DF}", suffix);
                                 temp.push(word);
                             } else {
-                                if item_rmc == "\u{09CE}" {
-                                    let word = format!("{}{}{}", &item[0..item.len()-1], "\u{09A4}", suffix);
+                                if item_rmc == '\u{09CE}' {
+                                    let word = format!(
+                                        "{}{}{}",
+                                        &item[0..item.len() - 1],
+                                        "\u{09A4}",
+                                        suffix
+                                    );
                                     temp.push(word);
-                                } else if item_rmc == "\u{0982}" {
-                                    let word = format!("{}{}{}", &item[0..item.len()-1], "\u{0999}", suffix);
+                                } else if item_rmc == '\u{0982}' {
+                                    let word = format!(
+                                        "{}{}{}",
+                                        &item[0..item.len() - 1],
+                                        "\u{0999}",
+                                        suffix
+                                    );
                                     temp.push(word);
                                 } else {
                                     let word = format!("{}{}", item, suffix);
@@ -53,28 +63,117 @@ impl PhoneticSuggestion {
                 }
             }
         }
-        
+
         temp
     }
 
     /// Make suggestion from the given `word`.
     pub(crate) fn suggest(&self, word: &str) -> Vec<String> {
-        let splitted_string = word.split_string();
+        let splitted_string = split_string(word);
         Vec::new()
     }
 }
 
+/// Split the string into three parts.
+/// This function splits preceding and trailing meta characters.
+fn split_string(input: &str) -> (String, String, String) {
+    let meta = "-]~!@#%&*()_=+[{}'\";<>/?|.,";
+    let mut first_index = 0;
+    let mut last_index = 0;
+    let mut encountered_alpha = false;
+
+    for (index, c) in input.chars().enumerate() {
+        if !meta.contains(c) {
+            first_index = index;
+            encountered_alpha = true;
+            break;
+        }
+    }
+
+    // Corner case: If we haven't yet encountered an alpha or
+    // a numeric character, then the string has no middle part
+    // or last part we need. So return "" for them ;)
+    if !encountered_alpha {
+        return (input[..].to_owned(), "".to_owned(), "".to_owned());
+    }
+
+    for (index, c) in input.chars().rev().enumerate() {
+        if !meta.contains(c) {
+            last_index = input.len() - index - 1;
+            break;
+        }
+    }
+
+    let first_part = input[0..first_index].to_owned();
+    let middle_part = input[first_index..=last_index].to_owned();
+    let last_part = input[last_index + 1..].to_owned();
+
+    (first_part, middle_part, last_part)
+}
+
 #[cfg(test)]
 mod tests {
-    use rustc_hash::FxHashMap;
+    use super::split_string;
     use super::PhoneticSuggestion;
     use crate::phonetic::database::Database;
+    use rustc_hash::FxHashMap;
     #[test]
     fn test_suffix() {
         let mut cache: FxHashMap<String, Vec<String>> = FxHashMap::default();
-        cache.insert("computer".to_string(), vec!["কম্পিউটার".to_string()]);
-        let suggestion = PhoneticSuggestion { suggestions: Vec::new(), database: Database::new(), cache };
-        assert_eq!(suggestion.add_suffix(Vec::new(), &("".to_string(), "computere".to_string(), "".to_string())), vec!["কম্পিউটারে"]);
-        assert_eq!(suggestion.add_suffix(Vec::new(), &("".to_string(), "computergulo".to_string(), "".to_string())), vec!["কম্পিউটারগুলো"]);
+        cache.insert(
+            "computer".to_string(),
+            vec!["কম্পিউটার".to_string()],
+        );
+        let suggestion = PhoneticSuggestion {
+            suggestions: Vec::new(),
+            database: Database::new(),
+            cache,
+        };
+        assert_eq!(
+            suggestion.add_suffix(
+                Vec::new(),
+                &("".to_string(), "computere".to_string(), "".to_string())
+            ),
+            vec!["কম্পিউটারে"]
+        );
+        assert_eq!(
+            suggestion.add_suffix(
+                Vec::new(),
+                &("".to_string(), "computergulo".to_string(), "".to_string())
+            ),
+            vec!["কম্পিউটারগুলো"]
+        );
+    }
+
+    #[test]
+    fn test_split_string() {
+        assert_eq!(
+            split_string("[][][][]"),
+            ("[][][][]".to_owned(), "".to_owned(), "".to_owned())
+        );
+        assert_eq!(
+            split_string("t*"),
+            ("".to_owned(), "t".to_owned(), "*".to_owned())
+        );
+        assert_eq!(
+            split_string("1"),
+            ("".to_owned(), "1".to_owned(), "".to_owned())
+        );
+        assert_eq!(
+            split_string("#\"percent%sign\"#"),
+            (
+                "#\"".to_owned(),
+                "percent%sign".to_owned(),
+                "\"#".to_owned()
+            )
+        );
+        assert_eq!(
+            split_string("text"),
+            ("".to_owned(), "text".to_owned(), "".to_owned())
+        );
+        assert_eq!(
+            split_string(":)"),
+            ("".to_owned(), ":".to_owned(), ")".to_owned())
+        );
     }
 }
--- src/utility.rs
+++ src/utility.rs
@@ -1,73 +1,27 @@
-use regex::Regex;
-
-lazy_static! {
-    static ref VOWEL: Regex = Regex::new(r#"^[\u0985\u0986\u0987\u0988\u0989\u098A\u098B\u098F\u0990\u0993\u0994\u098C\u09E1\u09BE\u09BF\u09C0\u09C1\u09C2\u09C3\u09C7\u09C8\u09CB\u09CC]$"#).unwrap();
-    static ref KAR: Regex = Regex::new(r#"^[\u09BE\u09BF\u09C0\u09C1\u09C2\u09C3\u09C7\u09C8\u09CB\u09CC\u09C4]$"#).unwrap();
-    static ref CONSONANT: Regex = Regex::new(r#"^[\u0995\u0996\u0997\u0998\u0999\u099A\u099B\u099C\u099D\u099E\u099F\u09A0\u09A1\u09A2\u09A3\u09A4\u09A5\u09A6\u09A7\u09A8\u09AA\u09AB\u09AC\u09AD\u09AE\u09AF\u09B0\u09B2\u09B6\u09B7\u09B8\u09B9\u09CE\u09DC\u09DD\u09DF]$"#).unwrap();
-}
-
-/// Some utility functions which we implement on the `str` type.
+/// Some utility functions which we implement on the `char` type.
 pub(crate) trait Utility {
-    /// Checks the str for a vowel character.
+    /// Checks the char for a vowel character.
     fn is_vowel(&self) -> bool;
-    /// Checks the str for a kar character.
+    /// Checks the char for a kar character.
     fn is_kar(&self) -> bool;
-    /// Checks the str for a pure consonant character.
+    /// Checks the char for a pure consonant character.
     fn is_pure_consonant(&self) -> bool;
-    /// Split the string into three parts.
-    /// This function splits preceding and trailing meta characters.
-    fn split_string(&self) -> (String, String, String);
 }
 
-impl Utility for str {
-    /// Checks the str for a vowel character.
+impl Utility for char {
+    /// Checks the char for a vowel character.
     fn is_vowel(&self) -> bool {
-        VOWEL.is_match(self)
+        "\u{0985}\u{0986}\u{0987}\u{0988}\u{0989}\u{098A}\u{098B}\u{098F}\u{0990}\u{0993}\u{0994}\u{098C}\u{09E1}\u{09BE}\u{09BF}\u{09C0}\u{09C1}\u{09C2}\u{09C3}\u{09C7}\u{09C8}\u{09CB}\u{09CC}".contains(*self)
     }
 
-    /// Checks the str for a kar character.
+    /// Checks the char for a kar character.
     fn is_kar(&self) -> bool {
-        KAR.is_match(self)
+        "\u{09BE}\u{09BF}\u{09C0}\u{09C1}\u{09C2}\u{09C3}\u{09C7}\u{09C8}\u{09CB}\u{09CC}\u{09C4}".contains(*self)
     }
 
-    /// Checks the str for a pure consonant character.
+    /// Checks the char for a pure consonant character.
     fn is_pure_consonant(&self) -> bool {
-        CONSONANT.is_match(self)
-    }
-
-    fn split_string(&self) -> (String, String, String) {
-        let meta = "-]~!@#%&*()_=+[{}'\";<>/?|.,";
-        let mut first_index = 0;
-        let mut last_index = 0;
-        let mut encountered_alpha = false;
-
-        for (index, c) in self.chars().enumerate() {
-            if !meta.contains(c) {
-                first_index = index;
-                encountered_alpha = true;
-                break;
-            }
-        }
-
-        // Corner case: If we haven't yet encountered an alpha or
-        // a numeric character, then the string has no middle part
-        // or last part we need. So return "" for them ;)
-        if !encountered_alpha {
-            return (self[..].to_owned(), "".to_owned(), "".to_owned());
-        }
-
-        for (index, c) in self.chars().rev().enumerate() {
-            if !meta.contains(c) {
-                last_index = self.len() - index - 1;
-                break;
-            }
-        }
-
-        let first_part = self[0..first_index].to_owned();
-        let middle_part = self[first_index..=last_index].to_owned();
-        let last_part = self[last_index + 1..].to_owned();
-
-        (first_part, middle_part, last_part)
+        "\u{0995}\u{0996}\u{0997}\u{0998}\u{0999}\u{099A}\u{099B}\u{099C}\u{099D}\u{099E}\u{099F}\u{09A0}\u{09A1}\u{09A2}\u{09A3}\u{09A4}\u{09A5}\u{09A6}\u{09A7}\u{09A8}\u{09AA}\u{09AB}\u{09AC}\u{09AD}\u{09AE}\u{09AF}\u{09B0}\u{09B2}\u{09B6}\u{09B7}\u{09B8}\u{09B9}\u{09CE}\u{09DC}\u{09DD}\u{09DF}".contains(*self)
     }
 }
 
@@ -76,42 +30,10 @@ mod test {
     use super::Utility;
     #[test]
     fn test_utilities() {
-        assert!("আ".is_vowel());
-        assert!(!"ক".is_vowel());
-        assert!("া".is_kar());
-        assert!(!"আ".is_kar());
-        assert!("ক".is_pure_consonant());
-    }
-
-    #[test]
-    fn test_split_string() {
-        assert_eq!(
-            "[][][][]".split_string(),
-            ("[][][][]".to_owned(), "".to_owned(), "".to_owned())
-        );
-        assert_eq!(
-            "t*".split_string(),
-            ("".to_owned(), "t".to_owned(), "*".to_owned())
-        );
-        assert_eq!(
-            "1".split_string(),
-            ("".to_owned(), "1".to_owned(), "".to_owned())
-        );
-        assert_eq!(
-            "#\"percent%sign\"#".split_string(),
-            (
-                "#\"".to_owned(),
-                "percent%sign".to_owned(),
-                "\"#".to_owned()
-            )
-        );
-        assert_eq!(
-            "text".split_string(),
-            ("".to_owned(), "text".to_owned(), "".to_owned())
-        );
-        assert_eq!(
-            ":)".split_string(),
-            ("".to_owned(), ":".to_owned(), ")".to_owned())
-        );
+        assert!('আ'.is_vowel());
+        assert!(!'ক'.is_vowel());
+        assert!('া'.is_kar());
+        assert!(!'আ'.is_kar());
+        assert!('ক'.is_pure_consonant());
     }
 }
