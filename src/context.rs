a5802c10223f0d8bd59044e9f13e3a664f87d2b1
Muhammad Mominul Huque <mominul2082@gmail.com>
2019-03-09 02:55:08 +0600

    Use snake_case

--- src/context.rs
+++ src/context.rs
@@ -10,40 +10,40 @@ pub struct RitiContext {
 
 impl RitiContext {
     /// Get suggestion for key.
-    pub fn getSuggestionForKey(&self, key: u16, shift: bool, ctrl: bool, alt: bool) -> Suggestion {
-        self.method.borrow().getSuggestion(key, shift, ctrl, alt)
+    pub fn get_suggestion_for_key(&self, key: u16, shift: bool, ctrl: bool, alt: bool) -> Suggestion {
+        self.method.borrow().get_suggestion(key, shift, ctrl, alt)
     }
 
     /// Returns bit masked integer instructing how the IM should handle the special key(eg. BackSpace,
     /// Enter, Space etc.)
     /// 
     /// See the [`Constants`](index.html#constants) which are bit masked for more information.
-    pub fn handleSpecialKey(&self, key: u16) -> u8 {
-        self.method.borrow().handleSpecialKey(key)
+    pub fn handle_special_key(&self, key: u16) -> u8 {
+        self.method.borrow().handle_special_key(key)
     }
 }
 
 pub(crate) trait Method {
-    fn setLayout(&self, path: &str);
-    fn getSuggestion(&self, key: u16, shift: bool, ctrl: bool, alt: bool) -> Suggestion;
-    fn handleSpecialKey(&self, key: u16) -> u8;
+    fn set_layout(&self, path: &str);
+    fn get_suggestion(&self, key: u16, shift: bool, ctrl: bool, alt: bool) -> Suggestion;
+    fn handle_special_key(&self, key: u16) -> u8;
 }
 
 /// IM needs to accept the key.
 /// 
-/// Returned by the [`handleSpecialKey()`](struct.RitiContext.html#method.handleSpecialKey) function.
+/// Returned by the [`handleSpecialKey()`](struct.RitiContext.html#method.handle_special_key) function.
 pub const IM_KEY_ACCEPTED: u8 = 1 << 0;
 /// IM needs to commit the current suggestion.
 /// 
-/// Returned by the [`handleSpecialKey()`](struct.RitiContext.html#method.handleSpecialKey) function.
+/// Returned by the [`handleSpecialKey()`](struct.RitiContext.html#method.handle_special_key) function.
 pub const IM_COMMIT: u8 = 1 << 1;
 /// IM needs to update suggestions.
 /// 
-/// Returned by the [`handleSpecialKey()`](struct.RitiContext.html#method.handleSpecialKey) function,
+/// Returned by the [`handleSpecialKey()`](struct.RitiContext.html#method.handle_special_key) function,
 /// usually when the key is BackSpace.
 pub const IM_NEED_UPDATE: u8 = 1 << 2;
 /// IM needs to reset.
 /// 
-/// Returned by the [`handleSpecialKey()`](struct.RitiContext.html#method.handleSpecialKey) function,
+/// Returned by the [`handleSpecialKey()`](struct.RitiContext.html#method.handle_special_key) function,
 /// usually when the key is BackSpace.
 pub const IM_NEED_RESET: u8 = 1 << 3;
