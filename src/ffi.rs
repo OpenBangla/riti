2039b3038b63a271de5d9b7171d10cec94d3efde
Muhammad Mominul Huque <mominul2082@gmail.com>
2023-12-23 22:04:31 +0600

    Phonetic Method: Preserve user's selection if the keypress was a punctuation mark (#40)

--- src/ffi.rs
+++ src/ffi.rs
@@ -32,18 +32,24 @@ pub extern "C" fn riti_context_free(ptr: *mut RitiContext) {
     riti_free(ptr)
 }
 
+/// Generates suggestion for `key` press.
+/// 
+/// `modifier`: state of modifier keys
+/// `selection`: previously selected user selection index if available otherwise `0`.
+/// It is used to preserve user's candidate selection if the key is a punctuation character in Phonetic method.
 #[no_mangle]
 pub extern "C" fn riti_get_suggestion_for_key(
     ptr: *mut RitiContext,
     key: u16,
     modifier: u8,
+    selection: u8,
 ) -> *mut Suggestion {
     let context = unsafe {
         assert!(!ptr.is_null());
         &*ptr
     };
 
-    let suggestion = context.get_suggestion_for_key(key, modifier);
+    let suggestion = context.get_suggestion_for_key(key, modifier, selection);
 
     Box::into_raw(Box::new(suggestion))
 }
--- src/phonetic/method.rs
+++ src/phonetic/method.rs
@@ -80,11 +80,22 @@ impl Method for PhoneticMethod {
         &mut self,
         key: u16,
         _modifier: u8,
+        selection: u8,
         data: &Data,
         config: &Config,
     ) -> Suggestion {
-        self.buffer.push(keycode_to_char(key));
-        self.create_suggestion(data, config)
+        let character = keycode_to_char(key);
+        self.buffer.push(character);
+        let mut suggestion = self.create_suggestion(data, config);
+
+        // Preserve user's selection if the keypress was a punctuation mark
+        if let Suggestion::Full { selection: ref mut sel, .. } = suggestion {
+            if matches!(character, '.' | '?' | '!' | ',' | ':' | ';' | '-' | '_' | ')' | '}' | ']' | '\'' | '"') {
+                *sel = selection.into();
+            }
+        }
+
+        suggestion
     }
 
     fn candidate_committed(&mut self, index: usize, config: &Config) {
@@ -168,6 +179,7 @@ mod tests {
     use crate::config::get_phonetic_method_defaults;
     use crate::context::Method;
     use crate::data::Data;
+    use crate::keycodes::{VC_COMMA, VC_R};
 
     #[test]
     fn test_backspace() {
@@ -187,5 +199,23 @@ mod tests {
             ..Default::default()
         };
         assert!(method.backspace_event(true, &data, &config).is_empty());
+    }
+
+    #[test]
+    fn test_preserve_selection() {
+        let config = get_phonetic_method_defaults();
+        let data = Data::new(&config);
+        let mut method = PhoneticMethod {
+            buffer: "coffee".to_string(),
+            ..Default::default()
+        };
+
+        let suggestion = method.get_suggestion(VC_COMMA, 0, 3, &data, &config);
+        assert_eq!(suggestion.previously_selected_index(), 3);
+
+        method.backspace_event(false, &data, &config);
+
+        let suggestion = method.get_suggestion(VC_R, 0, 3, &data, &config);
+        assert_eq!(suggestion.previously_selected_index(), 0);
     }
 }
